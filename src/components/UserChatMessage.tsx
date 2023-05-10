04500f6106939202bd4c0e637d17b93854ca425c
keitakn <keita.koga.work@gmail.com>
2023-05-10 18:25:35 +0900

    :art: #5 Userの吹き出しカラーをテーマカラーに合わせて変更

--- src/components/UserChatMessage.tsx
+++ src/components/UserChatMessage.tsx
@@ -11,7 +11,7 @@ export const UserChatMessage: FC<Props> = ({ message, avatarUrl, name }) => {
     <div className="chat-message">
       <div className="flex items-end justify-end">
         <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-1 items-end">
-          <div><span className="px-4 py-2 rounded-lg inline-block rounded-br-none bg-orange-200">{message}</span>
+          <div><span className="px-4 py-2 rounded-lg inline-block rounded-br-none bg-amber-200">{message}</span>
           </div>
         </div>
         <img
