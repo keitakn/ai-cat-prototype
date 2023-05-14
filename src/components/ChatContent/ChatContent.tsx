5389b90e20a2cddb6819098aafe0a2cad5bcc07a
keitakn <keita.koga.work@gmail.com>
2023-05-14 18:28:22 +0900

    :recycle: #14 ChatのHeader部分を別のComponentに分離

--- src/components/ChatContent/ChatContent.tsx
+++ src/components/ChatContent/ChatContent.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import type { FC, FormEvent } from 'react';
-import Image from 'next/image';
+import { ChatHeader } from '@/components/ChatContent/ChatHeader';
 import { ChatMessagesList, type ChatMessages } from './ChatMessagesList';
 
 type Props = {
@@ -15,29 +15,7 @@ export const ChatContent: FC<Props> = ({ chatMessages }) => {
 
   return (
     <>
-      <div className="flex justify-between border-b-2 border-amber-200 bg-yellow-200 py-3 sm:items-center">
-        <div className="relative flex items-center space-x-4">
-          <div className="relative">
-            <Image
-              src="https://lgtm-images.lgtmeow.com/2022/03/23/10/9738095a-f426-48e4-be8d-93f933c42917.webp"
-              // TODO width, heightの指定方法をどうするか後で考える
-              width={330}
-              height={400}
-              alt="もこちゃん"
-              className="h-10 w-10 rounded-full sm:h-16 sm:w-16"
-            />
-          </div>
-          <div className="flex flex-col leading-tight">
-            <div className="mt-1 flex items-center text-2xl">
-              <span className="mr-3 text-gray-700">もこちゃん</span>
-            </div>
-            <span className="text-lg text-gray-600">チンチラシルバー</span>
-          </div>
-        </div>
-        <div className="flex items-center space-x-2">
-          {/* Buttonとかを並べるエリア */}
-        </div>
-      </div>
+      <ChatHeader />
       <ChatMessagesList chatMessages={chatMessages} />
       <div className="mb-2 border-t-2 border-amber-200 bg-yellow-100 px-4 pt-4 sm:mb-0">
         <form
